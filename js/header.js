5b8e83c984ceb73083df8e71105130b6b4ff054f
Swayze <kovaloff10@gmail.com>
2023-03-31 00:30:41 +0300

    a

--- js/header.js
+++ js/header.js
@@ -1,36 +1,62 @@
-document.write(`<nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgba(0, 0, 0, .85);">
-<div class="container">
-  <a class="navbar-brand" href="#">
-    <img src="images/Logo.png" alt="Logo" height="41" class="mt-0 navbar-logo">
-    <span class="navbar-brand-name">
-      <span class="navbar-brand-name-1">skylight</span>
-      <span class="navbar-brand-name-2 text-uppercase text-decoration-none text-white"><br>media production agency</span>
-    </span>
-  </a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
-    <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="font-family: DIN Pro; font-weight: 900;font-size:18px;">
-      <li class="nav-item ">
-        <a class="nav-link text-white tlganim" aria-current="page" href="#">how we do it</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-white tlganim" aria-current="page" href="#">video</a>
-      </li>
-      <li class="nav-item"> 
-        <a class="nav-link text-white tlganim"  aria-current="page" href="#">photo</a>
-      </li>
-      <li class="nav-item" >
-        <a class="nav-link text-white tlganim" aria-current="page" href="#">video marketing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-white tlganim" aria-current="page" href="#">our team</a>
-      </li>
-      <li class="nav-item get-in-touch">
-        <a class="nav-link text-dark" aria-current="page" href="#">get in touch</a>
-      </li>
-    </ul>
+document.write(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/bootstrap.min.css">
+  <link rel="stylesheet" href="../fonts/fonts.css">
+  <link rel="stylesheet" href="..//styles/header.css">
+  <title>Document</title>
+  <style>
+    
+  </style>
+
+</head>
+
+<body style="background-color: aqua;">
+  <header>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgba(0, 0, 0, .85);">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="images/Logo.png" alt="Logo" height="41" class="mt-0 navbar-logo">
+      <span class="navbar-brand-name">
+        <span class="navbar-brand-name-1">skylight</span>
+        <span class="navbar-brand-name-2 text-uppercase text-decoration-none text-white"><br>media production agency</span>
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="font-family: DIN Pro; font-weight: 900;font-size:18px;">
+        <li class="nav-item ">
+          <a class="nav-link text-white tlganim" aria-current="page" href="#">how we do it</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white tlganim" aria-current="page" href="#">video</a>
+        </li>
+        <li class="nav-item"> 
+          <a class="nav-link text-white tlganim"  aria-current="page" href="#">photo</a>
+        </li>
+        <li class="nav-item" >
+          <a class="nav-link text-white tlganim" aria-current="page" href="#">video marketing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white tlganim" aria-current="page" href="#">our team</a>
+        </li>
+        <li class="nav-item get-in-touch">
+          <a class="nav-link text-dark" aria-current="page" href="#">get in touch</a>
+        </li>
+      </ul>
+    </div>
   </div>
-</div>
-</nav>`);
+  </nav>
+  </header>
+
+
+  <script src="bootstrap.bundle.min.js"></script>
+</body>
+
+</html>`);
