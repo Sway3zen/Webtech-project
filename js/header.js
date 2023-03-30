af5a6d160ca72a1574582b6f9c24e2a0c578b7cd
Swayze <kovaloff10@gmail.com>
2023-03-31 00:25:41 +0300

    a

---
+++ js/header.js
@@ -0,0 +1,36 @@
+document.write(`<nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgba(0, 0, 0, .85);">
+<div class="container">
+  <a class="navbar-brand" href="#">
+    <img src="images/Logo.png" alt="Logo" height="41" class="mt-0 navbar-logo">
+    <span class="navbar-brand-name">
+      <span class="navbar-brand-name-1">skylight</span>
+      <span class="navbar-brand-name-2 text-uppercase text-decoration-none text-white"><br>media production agency</span>
+    </span>
+  </a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="font-family: DIN Pro; font-weight: 900;font-size:18px;">
+      <li class="nav-item ">
+        <a class="nav-link text-white tlganim" aria-current="page" href="#">how we do it</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white tlganim" aria-current="page" href="#">video</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link text-white tlganim"  aria-current="page" href="#">photo</a>
+      </li>
+      <li class="nav-item" >
+        <a class="nav-link text-white tlganim" aria-current="page" href="#">video marketing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white tlganim" aria-current="page" href="#">our team</a>
+      </li>
+      <li class="nav-item get-in-touch">
+        <a class="nav-link text-dark" aria-current="page" href="#">get in touch</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>`);
