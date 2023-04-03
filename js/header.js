571a5ff32226d75f41d3caf46bdda17690b587a5
Swayze <kovaloff10@gmail.com>
2023-04-03 13:36:04 +0300

    s

--- js/header.js
+++ js/header.js
@@ -1,37 +1,39 @@
+
 document.write(`
-  <nav class="navbar navbar-expand-xl navbar-light position-relative" style="background-color: rgba(0, 0, 0, .85); width:100%; z-index:2;">
-  <div class="container">
-    <a class="navbar-brand" href="#header">
-      <img src="images/Logo.png" alt="Logo" height="41" class="mt-0 navbar-logo">
-      <span class="navbar-brand-name">
-        <span class="navbar-brand-name-1">skylight</span>
-        <span class="navbar-brand-name-2 text-uppercase text-decoration-none text-white"><br>media production agency</span>
-      </span>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="font-family: DIN Pro; font-weight: 900;font-size:18px;">
-        <li class="nav-item ">
-          <a class="nav-link text-white tlganim" aria-current="page" href="#">how we do it</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white tlganim" aria-current="page" href="#">video</a>
-        </li>
-        <li class="nav-item"> 
-          <a class="nav-link text-white tlganim"  aria-current="page" href="#">photo</a>
-        </li>
-        <li class="nav-item" >
-          <a class="nav-link text-white tlganim" aria-current="page" href="#">video marketing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white tlganim" aria-current="page" href="#">our team</a>
-        </li>
-        <li class="nav-item get-in-touch">
-          <a class="nav-link text-dark" aria-current="page" href="#">get in touch</a>
-        </li>
-      </ul>
-    </div>
+
+<nav class="navbar navbar-expand-xl navbar-light position-relative" style="background-color: rgba(0, 0, 0, .80); width:100%; z-index:2;">
+<div class="container">
+  <a class="navbar-brand" href="#header">
+    <img src="images/Logo.png" alt="Логотип" height="41" class="mt-0 navbar-logo">
+    <span class="navbar-brand-name">
+      <span class="navbar-brand-name-1">skylight</span>
+      <span class="navbar-brand-name-2 text-uppercase text-decoration-none text-white"><br>media production agency</span>
+    </span>
+  </a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Перемикач навігації">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="font-family: DIN Pro; font-weight: 900;font-size:18px;">
+      <li class="nav-item ">
+        <a class="nav-link text-white tlganim" aria-current="page" href="#">про нас</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white tlganim" aria-current="page" href="#">відео</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link text-white tlganim"  aria-current="page" href="#">фото</a>
+      </li>
+      <li class="nav-item" >
+        <a class="nav-link text-white tlganim" aria-current="page" href="#">відео-маркетинг</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white tlganim" aria-current="page" href="#">наша команда</a>
+      </li>
+      <li class="nav-item get-in-touch" >
+        <a class="nav-link text-dark" aria-current="page" href="#">напишіть нам</a>
+      </li>
+    </ul>
   </div>
-  </nav>`);
+</div>
+</nav>`);
