1337869f3a833a5a55aac41c00e1964115c758d6
Swayze <kovaloff10@gmail.com>
2023-06-05 20:42:13 +0300

    jk

--- public/js/header.js
+++ public/js/header.js
@@ -1,18 +1,18 @@
 
 document.write(`
 
-<nav class="navbar navbar-expand-xl navbar-light position-relative" style="background-color: rgba(0, 0, 0, .80); width:100%; z-index:2;">
+<nav class="navbar navbar-expand-xl navbar-light" style="background-color: rgba(0, 0, 0, .80); width:100%; z-index:2;">
 <div class="container">
   <a class="navbar-brand" href="index.html">
     <img src="images/Logo.png" alt="Логотип" height="41" class="mt-0 navbar-logo">
-    <span class="navbar-brand-name">
+    <div class="navbar-brand-name">
       <span class="navbar-brand-name-1">skylight</span>
       <span class="navbar-brand-name-2 text-uppercase text-decoration-none text-white"><br>media production agency</span>
-    </span>
+    </div>
   </a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Перемикач навігації">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
   <div class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
     <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style="font-family: DIN Pro; font-weight: 900;font-size:18px;">
       <li class="nav-item ">
