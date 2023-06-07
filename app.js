fbf2a8ac20de026b2510b33b6eb4561067fe7229
Swayze <kovaloff10@gmail.com>
2023-06-07 22:42:53 +0300

    admin form

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((res) => console.log('Connected to MongoDB'))
   .catch((error) => console.log(error));
 
-  const feedbackSchema = new Schema({
-    name: String,
-    phone: String,
-    email: String,
-    message: String
-  });
+const feedbackSchema = new Schema({
+  name: String,
+  phone: String,
+  email: String,
+  message: String
+});
 
-  const Feedback = mongoose.model(`Feedback's`, feedbackSchema);
+const Feedback = mongoose.model(`Feedback's`, feedbackSchema);
 
 const app = express();
 app.engine('html', require('ejs').renderFile);
@@ -29,55 +29,57 @@ const port = 3000;
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.post('/submit-feedback', urlencodedParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-  
-    const { name, phone, email, message } = req.body;
-  
-    const feedback = new Feedback({
-      name: name,
-      phone: phone,
-      email: email,
-      message: message
-    });
-  
-    feedback.save()
-      .then(() => {
-        console.log('Feedback saved successfully');
-        res.send('Дякуємо за зворотній зв\'язок!');
-      })
-      .catch(error => {
-        console.error('Failed to save feedback:', error);
-        res.sendStatus(500);
-      });
+  if (!req.body) return res.sendStatus(400);
+
+  const { name, phone, email, message } = req.body;
+
+  const feedback = new Feedback({
+    name: name,
+    phone: phone,
+    email: email,
+    message: message
   });
 
-  const authlogin = "Swayze"
-  const authpassword = "nazar"
+  feedback.save()
+    .then(() => {
+      console.log('Feedback saved successfully');
+      res.send('Дякуємо за зворотній зв\'язок!');
+    })
+    .catch(error => {
+      console.error('Failed to save feedback:', error);
+      res.sendStatus(500);
+    });
+});
 
-  app.post('/submit-login', urlencodedParser, function (req,res) {
-    if (!req.body) return res.sendStatus(400);
+const authlogin = "Swayze";
+const authpassword = "nazar";
 
-    const { login, password} = req.body;
-    if (login === authlogin && password === authpassword) {
-      console.log(login);
-      console.log(password);
-      res.send("Дякую за вхід");
+app.post('/submit-login', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+
+  const { login, password } = req.body;
+  if (login === authlogin && password === authpassword) {
+    console.log(login);
+    console.log(password);
+    try {
+      res.redirect('\\public\\admin.html');
     }
-    else {
-      alert("I can`t find this user.")
+    catch (e) {
+      console.log(e);
     }
-
-  
-  })
+  } else {
+    res.send("I can't find this user.");
+  }
+});
 
 app.use(serveStatic(path.join(__dirname, 'public')));
-app.use('/styles', express.static(__dirname + 'public/styles'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/images', express.static(__dirname + 'public/images'));
-app.use('/fonts', express.static(__dirname + 'public/fonts'));
+app.use('/styles', express.static(path.join(__dirname, 'public', 'styles')));
+app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
+app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
+app.use('/fonts', express.static(path.join(__dirname, 'public', 'fonts')));
 
 app.get('', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => console.info(`Listening on port ${port}`));
