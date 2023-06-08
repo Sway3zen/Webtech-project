9e677253d52707ee240ffb45d643f0615dc1003e
Swayze <kovaloff10@gmail.com>
2023-06-08 22:54:36 +0300

    Back-end done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
@@ -23,7 +24,9 @@ const Feedback = mongoose.model(`Feedback's`, feedbackSchema);
 
 const app = express();
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 const port = 3000;
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -54,25 +57,96 @@ app.post('/submit-feedback', urlencodedParser, function (req, res) {
 const authlogin = "Swayze";
 const authpassword = "nazar";
 
-app.post('/submit-login', urlencodedParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400);
+/*app.get('/admin.html', (req, res) => {
+  const { authorized } = req.query;
 
-  const { login, password } = req.body;
-  if (login === authlogin && password === authpassword) {
-    console.log(login);
-    console.log(password);
-    try {
-      res.redirect('\\public\\admin.html');
-    }
-    catch (e) {
-      console.log(e);
-    }
+  if (authorized === 'true') {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+  } else {
+    res.redirect('/loginform.html');
+  }
+});
+
+app.get('/submit-login', function (req, res) {
+  const { userlogin, userpassword } = req.query;
+
+  if (userlogin === authlogin && userpassword === authpassword) {
+    console.log(userlogin);
+    console.log(userpassword);
+    res.redirect('/admin.html?authorized=true');
+  } else {
+    res.send("I can't find this user.");
+  }
+});*/
+
+// app.get('/submit-login', function (req, res) {
+//   const { userlogin, userpassword } = req.query;
+
+//   if (userlogin === authlogin && userpassword === authpassword) {
+
+//     fs.readFile(path.join(__dirname, 'public', 'admin.html'), 'utf8', function (err, data) {
+//       if (err) {
+//         console.error('Failed to read admin.html:', err);
+//         res.sendStatus(500);
+//       } else {
+//         res.send(data);
+//       }
+//     });
+//   } else {
+//     res.send("I can't find this user.");
+//   }
+// });
+
+// app.get('/submit-login', function (req, res) {
+//   const { userlogin, userpassword } = req.query;
+
+//   if (userlogin === authlogin && userpassword === authpassword) {
+//     res.redirect('/feedbacks');
+//   } else {
+//     res.send("I can't find this user.");
+//   }
+// });
+
+
+// app.get('/feedbacks', function (req, res) {
+//   Feedback.find({})
+//     .then(feedbacks => {
+//       res.render('feedbacks', { feedbacks: feedbacks });
+//     })
+//     .catch(error => {
+//       console.error('Failed to fetch feedbacks:', error);
+//       res.sendStatus(500);
+//     });
+// });
+
+function authenticate(req, res, next) {
+  const { userlogin, userpassword } = req.query;
+
+  if (userlogin === authlogin && userpassword === authpassword) {
+    next(); 
+  } else {
+    res.redirect('/loginform.html'); 
+  }
+}
+
+
+app.get('/feedbacks', authenticate, function (req, res) {
+  res.render('feedbacks');
+});
+
+app.get('/submit-login', function (req, res) {
+  const { userlogin, userpassword } = req.query;
+
+  if (userlogin === authlogin && userpassword === authpassword) {
+    res.redirect('/feedbacks'); 
   } else {
     res.send("I can't find this user.");
   }
 });
 
-app.use(serveStatic(path.join(__dirname, 'public')));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/styles', express.static(path.join(__dirname, 'public', 'styles')));
 app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
 app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
